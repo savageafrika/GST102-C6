058f41dd1760f87dbe1c5d215e3dd113e41b39f8
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:05:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,587 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: " ___ is a short piece of writing that discusses, describe the analyses one topic",
+
+         answer : "Essay",
+
+         options : [
+
+                          "Essay",
+
+                          "Prose",
+
+                          "Speech writing",
+
+                          "Novel"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "Every good essay must be marked by these except",
+
+         answer : "Variety",
+
+         options : [
+
+                          "Clarity",
+
+                          "Unity",
+
+                          "Variety",
+
+                          "Coherence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "A good essayist should have adequate knowledge of",
+
+         answer : "Paragraph development",
+
+         options : [
+
+                          "Topic sentence",
+
+                          "Sentence construction",
+
+                          "Punctuation marks",
+
+                          "Paragraph development"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "___ gives the reader idea of what the essay is all about",
+
+         answer : "Introduction",
+
+         options : [
+
+                          "Prologue",
+
+                          "Introduction",
+
+                          "Subject matter",
+
+                          "Thesis statement"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "An introduction of an essay leads the reader to",
+
+         answer : "Thesis statement",
+
+         options : [
+
+                          "Main body",
+
+                          "Subject matter",
+
+                          "Thesis statement",
+
+                          "Narrative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "___ purports the writer’s point of view",
+
+         answer : "Thesis statement",
+
+         options : [
+
+                          "Main body",
+
+                          "Subject matter",
+
+                          "Thesis statement",
+
+                          "Narrative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "The logical order of ideas include these except",
+
+         answer : "Coherent order",
+
+         options : [
+
+                          "Chronological order",
+
+                          "Spatial order",
+
+                          "Emphatic order",
+
+                          "Coherent order"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "In which logical ordering of ideas is the least important idea presented before the most important one",
+
+         answer : "Emphatic order",
+
+         options : [
+
+                          "Chronological order",
+
+                          "Spatial order",
+
+                          "Emphatic order",
+
+                          "Coherent order"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "In a narrative essay the writer present his ideas in __ order",
+
+         answer : "Chronological order",
+
+         options : [
+
+                          "Chronological order",
+
+                          "Spatial order",
+
+                          "Emphatic order",
+
+                          "Coherent order"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "The narrative essay is ___ in style",
+
+         answer : "Conversational",
+
+         options : [
+
+                          "Conversational",
+
+                          "Expressive",
+
+                          "Story telling",
+
+                          "Figurative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "The descriptive essay presents a __ of a subject in the mind of the reader",
+
+         answer : "Pen picture",
+
+         options : [
+
+                          "Distinct image",
+
+                          "Clear idea",
+
+                          "Impression",
+
+                          "Pen picture"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "___ type of essay is not influenced by writer’s feelings, prejudices, attitudes or impressions.",
+
+         answer : "Objective description",
+
+         options : [
+
+                          "Objective description",
+
+                          "Subjective description",
+
+                          "Perceptual description",
+
+                          "Logical description"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "___ type of essay lacks authenticity of facts and figures",
+
+         answer : "Subjective description",
+
+         options : [
+
+                          "Objective description",
+
+                          "Subjective description",
+
+                          "Perceptual description",
+
+                          "Logical description"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:14,
+
+         question: "Which of these is an example of a descriptive essay topic",
+
+         answer : "A football match",
+
+         options : [
+
+                          "An accident I witnessed",
+
+                          "A memorable day in my life",
+
+                          "The election day in my town",
+
+                          "A football match"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:15,
+
+         question: "Which of these is an example of a narrative essay topic",
+
+         answer : "My experience in the jungle",
+
+         options : [
+
+                          "My experience in the jungle",
+
+                          "A place I would like to visit again",
+
+                          "My English lecturer",
+
+                          "The person I admire very much"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:16,
+
+         question: "In argumentative essay__ is the watchword",
+
+         answer : "Logicality",
+
+         options : [
+
+                          "Persuasion",
+
+                          "Logicality",
+
+                          "Coercion",
+
+                          "Validity"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:17,
+
+         question: "In what kind of argument does the writer appeal to the reasoning of the audience through logic",
+
+         answer : "Pure argument",
+
+         options : [
+
+                          "Deduction",
+
+                          "Pure argument",
+
+                          "Persuasive writing",
+
+                          "Inductive reasoning"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:18,
+
+         question: "In what kind of argument does the writer excite the emotions of his audience",
+
+         answer : "Persuasive writing",
+
+         options : [
+
+                          "Deduction",
+
+                          "Pure argument",
+
+                          "Persuasive writing",
+
+                          "Inductive reasoning"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:19,
+
+         question: "The primary function of an expository essay is not to convince or persuade the reader but to __",
+
+         answer : "Explain something",
+
+         options : [
+
+                          "Describe something",
+
+                          "Narrate something",
+
+                          "Expose something",
+
+                          "Explain something"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:20,
+
+         question: "Which of these is not a step involved in pre writing stage of an essay",
+
+         answer : "Essay restructuring",
+
+         options : [
+
+                          "Choosing a topic",
+
+                          "Generating ideas",
+
+                          "Essay restructuring",
+
+                          "Developing the thesis statement"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:21,
+
+         question: "Criteria to be considered for topic selection in essay writing include these except",
+
+         answer : "Type of essay",
+
+         options : [
+
+                          "Careful Reading",
+
+                          "Knowledge of the topic",
+
+                          "Interest",
+
+                          "Type of essay"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:22,
+
+         question: "The hallmark of a well-organised essay is a ___",
+
+         answer : "Thesis support structure",
+
+         options : [
+
+                          "Thesis support structure",
+
+                          "Adequate paragraphing",
+
+                          "A good topic",
+
+                          "Logical coherence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:23,
+
+         question: "The best time to think about how to organise your paper is during the__",
+
+         answer : "The prewriting stage",
+
+         options : [
+
+                          "The prewriting stage",
+
+                          "The publishing stage",
+
+                          "The organising stage",
+
+                          "The writing stage"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:24,
+
+         question: "The last stage of essay writing is",
+
+         answer : "The publishing stage",
+
+         options : [
+
+                          "The prewriting stage",
+
+                          "The publishing stage",
+
+                          "The editing stage",
+
+                          "The writing stage"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
